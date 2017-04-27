f36781f95539940f69e38ccb8d5030444e1607e1
brian.yang <brian.yang@qunar.com>
2017-04-27 18:45:23 +0800

    Revert "抓取代理ip以及bt天堂的种子信息"
    This reverts commit 06191e0843f12043f096297136c7061107688c57.

--- btcrawl/__init__.py
+++
@@ -1,7 +0,0 @@
-# encoding:utf8
-__author__ = 'brianyang'
-
-import os
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
--- btcrawl/bt_config.py
+++
@@ -1,9 +0,0 @@
-# encoding:utf8
-__author__ = 'brianyang'
-
-import os
-import sys
-
-sys.setrecursionlimit(5000)
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
